fbd74c9baec573f806d2a63303e3121f3d7c9246
AdityaaMishraa <75307682+AdityaaMishraa@users.noreply.github.com>
2021-05-04 19:19:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas;
+var allContestants;
+var gameState = 0;
+var contestantCount,database;
+var quiz,question,contestant;
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+ 
+  question = new Question();
+}
+
+
+function draw(){
+  background("pink");
+if(contestantCount===2){
+  quiz.update(1);
+}
+if(gameState===1){
+  clear();
+  quiz.play();
+}
+  
+}
